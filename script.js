fc9b46372d7f3af1400472f7316ffaf122ce1935
Pawan Shrestha <pawanshresthait@gmail.com>
2021-06-18 13:16:45 +0545

    Added Functionality

--- script.js
+++ script.js
@@ -2,3 +2,18 @@ const openBtn = document.querySelector('#open-modal-btn');
 const overlay = document.querySelector('#overlay');
 const modal = document.querySelector('#modal');
 const closeBtn = modal.querySelector('#close-modal-btn');
+
+openBtn.addEventListener('click', e => {
+  overlay.classList.add('open');
+  modal.classList.add('open');
+});
+
+closeBtn.addEventListener('click', e => {
+  overlay.classList.remove('open');
+  modal.classList.remove('open');
+});
+
+overlay.addEventListener('click', e => {
+  overlay.classList.remove('open');
+  modal.classList.remove('open');
+});
