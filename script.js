b20bd5fdbe3c3a07724044a82242a681a1a071ca
Pawan Shrestha <pawanshresthait@gmail.com>
2021-06-18 13:14:12 +0545

    Selected all Elements

---
+++ script.js
@@ -0,0 +1,4 @@
+const openBtn = document.querySelector('#open-modal-btn');
+const overlay = document.querySelector('#overlay');
+const modal = document.querySelector('#modal');
+const closeBtn = modal.querySelector('#close-modal-btn');
